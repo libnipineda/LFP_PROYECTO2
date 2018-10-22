c4c5263704dfa057e1ae07102c4fd3dfb566d18e
libnipineda <libnipineda@hotmail.com>
2018-10-22 05:54:30 -0600

    v1.2

---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _LFP_Proyecto2
+{
+    public partial class Form1 : Form
+    {
+        Lexico lexico = new Lexico();        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog abrir = new OpenFileDialog();
+            abrir.Filter = "Archivo|*.DIS";
+            abrir.Title = "Abrir Archivo";
+            abrir.FileName = "Sin titulo";
+            var dato = abrir.ShowDialog();
+               if (dato == DialogResult.OK)
+               {
+                StreamReader leer = new StreamReader(abrir.FileName);
+                richTextBox1.Text = leer.ReadToEnd();
+                leer.Close();
+               }
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // mejorar forma de guardar
+            SaveFileDialog guardar = new SaveFileDialog();
+            guardar.Filter = "Archivo|*.DIS";
+            guardar.Title = "Guardar Como";
+            guardar.FileName = "Sin Titulo";
+            var dato = guardar.ShowDialog();
+            if (dato == DialogResult.OK)
+            {
+                StreamWriter escribir = new StreamWriter(guardar.FileName);
+                foreach (object item in richTextBox1.Lines)
+                {
+                    escribir.WriteLine(item);
+                }
+                escribir.Close();
+            }
+        }
+
+        private void guardarComoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog guardar = new SaveFileDialog();
+            guardar.Filter = "Archivo|*.DIS";
+            guardar.Title = "Guardar Como";
+            guardar.FileName = "Sin Titulo";
+            var dato = guardar.ShowDialog();
+               if (dato == DialogResult.OK)
+               {
+                  StreamWriter escribir = new StreamWriter(guardar.FileName);
+                  foreach (object item in richTextBox1.Lines)
+                  {
+                    escribir.WriteLine(item);
+                  }
+                escribir.Close();
+               }
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Desea salir de la aplicación", "SALIR", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void analizarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lexico.Analisis(richTextBox1.Text);
+            //lexico.Tablatkn();
+            //lexico.TablaEtkn();
+        }
+
+        private void tableroDeJuegoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lexico.Ejecutar();
+            //Form2 frm = new Form2();
+            //frm.ShowDialog();
+            //if (frm.DialogResult == DialogResult.Yes)
+            //{
+
+            //}
+        }
+
+        private void manualDeUsuarioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+         //   Process.Start(@"C: \Users\Aarón\Documents\Lenguajes\Proyecto 2\[LFP]Proyecto2_201403541\Manual Tecnico.pdf");
+        }
+
+        private void manualTecnicoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+         //   Process.Start(@"C: \Users\Aarón\Documents\Lenguajes\Proyecto 2\[LFP]Proyecto2_201403541\Manual de Usuario.pdf");
+        }
+
+        private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "LFP PROYECTO\n Uzzi Libni Aarón Pineda Solórzano\n carné:201403541 \n Sección: A-",
+                "Acerca de...");
+        }
+    }
+}
---
+++ Funcionalidad.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _LFP_Proyecto2
+{    
+    class Funcionalidad
+    {
+        int DimX = 0, DimY = 0;
+
+        public void Principal(List<Lista> listas)
+        {            
+            
+        }
+
+        public void Nivel(List<Lista> listas)
+        {
+            Boolean num = false, numx = false, num2= false, numy=false;
+            foreach (var item in listas)
+            {
+                if (item.numero.Equals("5"))
+                {
+                    num = true;
+                }
+                if (num) // encontro la palabra reservada
+                {
+                    if (item.numero.Equals("18"))
+                    {                        
+                        numx = true;
+                        num = false;
+                    }
+                }
+                if (numx)
+                {
+                   try
+                   {
+                        if (item.tkn.Equals("Numero"))
+                        {
+                            DimX = Convert.ToInt32(item.lexema);
+                            DimensionForm();
+                            numx = false;
+                        }                        
+                   }
+                   catch (Exception)
+                   {
+                        DimensionForm();
+                        throw;
+                   }
+                }// Hasta aqui se acepta la coordenada x
+                 //---------------------------------------------------------------------------------------------------------------------------------------------------------------
+                if (item.numero.Equals("2"))
+                {
+                    num2 = true;
+                }
+                if (num2) // encontro la palabra reservada
+                {
+                    if (item.numero.Equals("22"))
+                    {
+                        numy = true;
+                        num2 = false;
+                    }
+                }
+                if (numy)
+                {
+                    try
+                    {
+                        if (item.tkn.Equals("Numero"))
+                        {
+                            DimY = Convert.ToInt32(item.lexema);
+                            DimensionForm();
+                            numy = false;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        DimensionForm();
+                        throw;
+                    }
+                }// Hasta aqui se acepta la coordenada y
+            } 
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------
+        }
+
+        public void Enemigo(List<Lista> listas)
+        {
+
+        }
+
+        public void Personaje(List<Lista> listas)
+        {
+
+        }
+
+        public void DimensionForm()
+        {
+            if (DimX > 3 & DimX <21 || DimY > 3 & DimY <16)
+            {
+                MessageBox.Show("Valores en x: " + DimX + "," + DimY);
+            }
+            else
+            {
+                MessageBox.Show("3 < x <= 20"  + "," + "3 < y <= 15");
+            }            
+        }
+    }
+}
